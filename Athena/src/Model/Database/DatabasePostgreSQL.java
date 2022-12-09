9409a24dbfc1a55c42ad3f757e55e663fe2dc20e
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2022-12-09 16:10:33 -0300

    Update DatabasePostgreSQL.java

--- Athena/src/Model/Database/DatabasePostgreSQL.java
+++ Athena/src/Model/Database/DatabasePostgreSQL.java
@@ -13,7 +13,7 @@ public class DatabasePostgreSQL implements Database {
     public Connection conectar() {
         try {
             Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/athena_bd", "postgres","Nicolas@2022");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/athena_bd", "postgres","postgres");
             return this.connection;
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DatabasePostgreSQL.class.getName()).log(Level.SEVERE, null, ex);
